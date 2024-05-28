75e74705850e423d4c83d4d6fb71bc8574425b78
Juergen Enge <you@example.com>
2024-05-28 17:58:39 +0200

    pinger.go created

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,20 +1,16 @@
 package client
 
 import (
-	"context"
 	"crypto/tls"
 	"emperror.dev/errors"
 	"fmt"
-	genericproto "github.com/je4/genericproto/v2/pkg/generic/proto"
 	"github.com/je4/miniresolver/v2/pkg/grpchelper"
 	pb "github.com/je4/miniresolver/v2/pkg/miniresolverproto"
 	"github.com/je4/utils/v2/pkg/zLogger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/types/known/emptypb"
 	"io"
-	"reflect"
 	"time"
 )
 
@@ -76,21 +72,4 @@ func NewMiniresolverClient(serverAddr string, tlsConfig *tls.Config, resolverTim
 		tlsConfig:          tlsConfig,
 		opts:               opts,
 	}, nil
-}
-
-type GRPCPinger interface {
-	Ping(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*genericproto.DefaultResponse, error)
-}
-
-func DoPing(pinger GRPCPinger, logger zLogger.ZLogger) {
-	pingerType := reflect.TypeOf(pinger)
-	if resp, err := pinger.Ping(context.Background(), &emptypb.Empty{}); err != nil {
-		logger.Error().Msgf("cannot ping %s: %v", pingerType.String(), err)
-	} else {
-		if resp.GetStatus() != genericproto.ResultStatus_OK {
-			logger.Error().Msgf("cannot ping %s: %v", pingerType.String(), resp.GetStatus())
-		} else {
-			logger.Info().Msgf("%s ping response: %s", pingerType.String(), resp.GetMessage())
-		}
-	}
 }
---
+++ pkg/client/pinger.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"context"
+	genericproto "github.com/je4/genericproto/v2/pkg/generic/proto"
+	"github.com/je4/utils/v2/pkg/zLogger"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"reflect"
+)
+
+type GRPCPinger interface {
+	Ping(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*genericproto.DefaultResponse, error)
+}
+
+func DoPing(pinger GRPCPinger, logger zLogger.ZLogger) {
+	pingerType := reflect.TypeOf(pinger)
+	if resp, err := pinger.Ping(context.Background(), &emptypb.Empty{}); err != nil {
+		logger.Error().Msgf("cannot ping %T: %v", pinger, err)
+	} else {
+		if resp.GetStatus() != genericproto.ResultStatus_OK {
+			logger.Error().Msgf("cannot ping %s: %v", pingerType.String(), resp.GetStatus())
+		} else {
+			logger.Info().Msgf("%s ping response: %s", pingerType.String(), resp.GetMessage())
+		}
+	}
+}
